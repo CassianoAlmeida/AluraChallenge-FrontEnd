b54e7204f7493462a69b98f5ff1d31219b8670ca
Sasa <none@none.com>
2022-04-09 15:02:39 -0300

    novo commit

--- js/model/pegaImagem.js
+++ js/model/pegaImagem.js
@@ -1,9 +1,3 @@
-const inputImagem = document.getElementById("image_input");
 
-var imagemCarregada = "";
-
-const carregaImagem = inputImagem.addEventListener("change", function(){
-    console.log(inputImagem.value);
-})
 
 export default all;
