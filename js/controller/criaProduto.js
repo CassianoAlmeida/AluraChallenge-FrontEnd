ce490c765ba23cfd453663b245f66177762fe520
Sasa <none@none.com>
2022-04-14 06:05:38 -0300

    desenvolvendo banco de dados e fazendo POST para o produto

--- js/controller/criaProduto.js
+++ js/controller/criaProduto.js
@@ -1,59 +1,61 @@
-import { getDBCards } from "./getDBCards.js";
-
-const criaProduto = async () => {
-    const list = await getDBCards();
-    return list
-}
-
-//console.log(criaProduto)
-
-const submit = document.querySelector('[data-submit]')
-
-const produto = submit.addEventListener('click', () => {
-    const nameInput = document.getElementById("nome").value;
-    const priceInput = document.getElementById("preco").value;
-    const selectInput = document.querySelector('select');
-    const option = selectInput.children[selectInput.selectedIndex];
-    const text = option.textContent;    
-    const descriptionInput = document.getElementById("descricao").value;
-    const produto = {
-        image: ImagemCarregadaValue,
-        name: nameInput, 
-        price: priceInput, 
-        category: text, 
-        description: descriptionInput
-    };
-    console.log(produto)
-    return produto;
-})
+//Captura a imagem arrastada para a drag-box-area e mostra preview
 
 const arrastaImagem = document.getElementById("image-input");
-const selecionaImagem = document.getElementById("")
-
-var imagemCarregada = "";
+let imagemCarregada = "";
 
 arrastaImagem.addEventListener("change", function(){
+    const preview = document.getElementById("preview")
     const reader = new FileReader();
     reader.addEventListener('load', () => {
-        imagemCarregada = reader.result;
-        document.getElementById('drag-box').style.backgroundImage = `url(${imagemCarregada})`;
+        preview.src = reader.result;
         document.getElementById("img-icon").classList.add('novo-produto__add-image__drag-box__icon-hide');
         document.getElementById("drag-box-description").classList.add('novo-produto__add-image__drag-box__text-hide');        
     });
     reader.readAsDataURL(this.files[0]);
-    console.log(imagemCarregada)
 })
 
+//Captura a imagem arrastada para a drag-box-area e mostra preview
 
+const selecionaImagem = document.getElementById("button-input")
 
-//inputImagem.addEventListener("change", function(){
-//    const reader = new FileReader();
-//    reader.addEventListener('load', () => {
-//        imagemCarregada = reader.result;
-//        document.getElementById('drag-box').style.backgroundImage = `url(${imagemCarregada})`;
-//        document.getElementById("img-icon").classList.add('novo-produto__add-image__drag-box__icon-hide');
-//        document.getElementById("drag-box-description").classList.add('novo-produto__add-image__drag-box__text-hide');        
-//        //console.log(imagemCarregada)
-//    });
-//    reader.readAsDataURL(this.files[0]);
-//})
+selecionaImagem.addEventListener("change", function(){
+    const preview = document.getElementById("preview")
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+        preview.src = reader.result;
+        document.getElementById("img-icon").classList.add('novo-produto__add-image__drag-box__icon-hide');
+        document.getElementById("drag-box-description").classList.add('novo-produto__add-image__drag-box__text-hide');
+    });
+    reader.readAsDataURL(this.files[0]);
+})
+
+//Envia formulário com o item para o db.json
+
+const submit = document.querySelector('[data-submit]')
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    const image = document.querySelector('#preview').getAttribute('src')
+    const nameInput = document.getElementById("nome").value;
+    const priceInput = document.getElementById("preco").value;
+    const selectInput = document.querySelector('select');
+    const option = selectInput.children[selectInput.selectedIndex];
+    const category = option.textContent;    
+    const descriptionInput = document.getElementById("descricao").value;
+    const produto = {
+        image: image,
+        name: nameInput, 
+        price: priceInput, 
+        category: category, 
+        description: descriptionInput
+    };
+    console.log(produto)
+    fetch('http://localhost:3000/produtos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(produto),
+    })
+})
+
