9533a081a20a228273a0e8671940993bef7d0c09
Sasa <none@none.com>
2022-08-18 21:25:49 -0300

    finalizando campo de pesquisa mobile no header

--- js/controller/searchBarProdutos.js
+++ js/controller/searchBarProdutos.js
@@ -1,8 +1,18 @@
 const botaoPesquisa = document.getElementById('button-search');
 const conteudoPesquisa = document.getElementById('input-search-bar');
 
+
 botaoPesquisa.addEventListener('click', () => {
     console.log(conteudoPesquisa.value);
     sessionStorage.setItem('search', `${conteudoPesquisa.value}`)
     window.location = './pesquisa.html';
+})
+
+
+conteudoPesquisa.addEventListener('keydown', (e) => {
+    if(e.key == 'Enter') {
+        console.log('oi')
+        sessionStorage.setItem('search', `${conteudoPesquisa.value}`)
+        window.location = './pesquisa.html';
+    }
 })
--- js/controller/searchBarProdutosIndex.js
+++ js/controller/searchBarProdutosIndex.js
@@ -1,8 +1,26 @@
 const botaoPesquisa = document.getElementById('button-search');
 const conteudoPesquisa = document.getElementById('input-search-bar');
+const imgPesquisa = document.getElementById('img-search');
+const botaoLogin = document.getElementById('login-button');
+const pesquisaContainer = document.getElementById('search-box');
 
 botaoPesquisa.addEventListener('click', () => {
-    console.log(conteudoPesquisa.value);
-    sessionStorage.setItem('search', `${conteudoPesquisa.value}`)
-    window.location = './views/pesquisa.html';
+    if(window.innerWidth > 668) {
+        sessionStorage.setItem('search', `${conteudoPesquisa.value}`)
+        window.location = './views/pesquisa.html';
+    } else {
+        imgPesquisa.className = "nav__container__search-box__img nav__container__search-box__img-hide";
+        botaoLogin.className = "nav__container__login-button-hide";
+        pesquisaContainer.className = "nav__container__search-box nav__container__search-box__mobile";
+        conteudoPesquisa.className = "nav__container__search-box__input nav__container__search-box__mobile__input";
+        conteudoPesquisa.focus();
+    }
+})
+
+conteudoPesquisa.addEventListener('keydown', (e) => {
+    if(e.key == 'Enter') {
+        console.log(window.innerWidth)
+        sessionStorage.setItem('search', `${conteudoPesquisa.value}`)
+        window.location = './views/pesquisa.html';
+    }
 })
