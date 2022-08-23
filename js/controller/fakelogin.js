2984309b055e5007c7f9fea5cccc79f93b2a6229
Sasa <none@none.com>
2022-08-23 19:42:31 -0300

    corrigindo rota login

--- js/controller/fakelogin.js
+++ js/controller/fakelogin.js
@@ -7,7 +7,7 @@ botaoLogin.addEventListener('click', () =>{
     const emailField = document.getElementById('email').value;
     const senhaFiled = document.getElementById('senha').value;
     if(emailField == email && senhaFiled == senha){
-        window.location.href = 'http://127.0.0.1:3000/views/produtos.html';
+        window.location = 'views/produtos.html';
     } else {
         alert('E-mail ou senha estão incorretos');
     }
